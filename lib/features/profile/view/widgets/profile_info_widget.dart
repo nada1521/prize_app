01d97447d25edb768fe7bc8521660dde7642bb6e
LAZEZ\lazez <lazezma7rous@gmail.com>
2025-07-02 14:48:52 +0300

    empty commit

--- lib/features/profile/view/widgets/profile_info_widget.dart
+++ lib/features/profile/view/widgets/profile_info_widget.dart
@@ -1,7 +1,9 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:prize/core/constant/app_svgs.dart';
+import 'package:prize/core/utils/generated/tr_locale_keys.g.dart';
 import 'package:prize/core/utils/helper/spacing.dart';
 import 'package:prize/core/utils/resources/app_circular_icon_button.dart';
 import 'package:prize/core/utils/resources/app_widget_color.dart';
@@ -34,6 +36,8 @@ class ProfileInfoWidget extends StatelessWidget {
             mainAxisSize: MainAxisSize.max,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
+
+              Text("${LocaleKeys.action.tr()} 1.3"),
               Expanded(
                 flex: 2,
                 child: Row(
