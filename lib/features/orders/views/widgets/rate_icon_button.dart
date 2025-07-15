f163257215433ecb4bbde908ee3eabfed28f6a29
DESKTOP-NL6E717\EL MAHDY 01007778867 <nada01027818814@gmail.com>
2025-07-15 22:35:36 +0200

    - compilet the localization for my order screen

--- lib/features/orders/views/widgets/rate_icon_button.dart
+++ lib/features/orders/views/widgets/rate_icon_button.dart
@@ -1,7 +1,9 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:prize/core/constant/app_svgs.dart';
+import 'package:prize/core/utils/generated/tr_locale_keys.g.dart';
 import 'package:prize/core/utils/helper/spacing.dart';
 import 'package:prize/core/utils/resources/app_colors.dart';
 import 'package:prize/core/utils/resources/app_text_styles.dart';
@@ -26,7 +28,7 @@ class RateIconButton extends StatelessWidget {
                 SvgPicture.asset(AppSvgs.rateStar),
                 horizontalSpace(5),
                 Text(
-                  "Rate",
+                  LocaleKeys.my_order_screen_rate.tr(),
                   style:
                       AppTextStyles.appEmptyFillColorButtonTextStyle(context),
                 ),
